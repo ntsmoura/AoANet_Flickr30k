59a435e7468b8986f2d720e3579db2a575a43131
natanmoura <natanmoura@escavador.com>
2024-04-15 10:00:08 -0300

    train.sh remodeling

--- train.sh
+++ train.sh
@@ -1,4 +1,7 @@
 id="aoanet"
+input_json = "../drive/MyDrive/Colab\ Notebooks/flickr30k/data/f30ktalk.json"
+input_label_h5 = "../drive/MyDrive/Colab\ Notebooks/flickr30k/data/f30ktalk_label.h5"
+input_att_dir = "../drive/MyDrive/Colab\ Notebooks/flickr30k/f30k_att/f30kbu_att.pth"
 if [ ! -f log/log_$id/infos_$id.pkl ]; then
 start_from=""
 else
@@ -17,11 +20,9 @@ python train.py --id $id \
     --ctx_drop 1 \
     --dropout_aoa 0.3 \
     --label_smoothing 0.2 \
-    --input_json data/cocotalk.json \
-    --input_label_h5 data/cocotalk_label.h5 \
-    --input_fc_dir  data/cocobu_fc \
-    --input_att_dir  data/cocobu_att  \
-    --input_box_dir  data/cocobu_box \
+    --input_json $input_json \
+    --input_label_h5 $input_label_h5 \
+    --input_att_dir $input_att_dir \
     --seq_per_img 5 \
     --batch_size 10 \
     --beam_size 1 \
@@ -40,38 +41,3 @@ python train.py --id $id \
     --scheduled_sampling_increase_every 5 \
     --scheduled_sampling_max_prob 0.5 \
     --learning_rate_decay_every 3
-
-python train.py --id $id \
-    --caption_model aoa \
-    --refine 1 \
-    --refine_aoa 1 \
-    --use_ff 0 \
-    --decoder_type AoA \
-    --use_multi_head 2 \
-    --num_heads 8 \
-    --multi_head_scale 1 \
-    --mean_feats 1 \
-    --ctx_drop 1 \
-    --dropout_aoa 0.3 \
-    --input_json data/cocotalk.json \
-    --input_label_h5 data/cocotalk_label.h5 \
-    --input_fc_dir  data/cocobu_fc \
-    --input_att_dir  data/cocobu_att  \
-    --input_box_dir  data/cocobu_box \
-    --seq_per_img 5 \
-    --batch_size 10 \
-    --beam_size 1 \
-    --num_layers 2 \
-    --input_encoding_size 1024 \
-    --rnn_size 1024 \
-    --language_eval 1 \
-    --val_images_use -1 \
-    --save_checkpoint_every 3000 \
-    --start_from log/log_$id \
-    --checkpoint_path log/log_$id"_rl" \
-    --learning_rate 2e-5 \
-    --max_epochs 40 \
-    --self_critical_after 0 \
-    --learning_rate_decay_start -1 \
-    --scheduled_sampling_start -1 \
-    --reduce_on_plateau 
